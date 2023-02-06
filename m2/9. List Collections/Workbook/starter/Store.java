f5ac80ee9803588bb941100d4890f9dbb276b3d9
Jake Lim <jake.lim@ams-osram.com>
2023-02-06 09:31:11 +0800

    176

--- m2/9. List Collections/Workbook/starter/Store.java
+++ m2/9. List Collections/Workbook/starter/Store.java
@@ -1,24 +1,26 @@
 import java.util.List;
+import java.util.LinkedList;
 
 public class Store {
 
     private List<Movie> movies;
 
     public Store() {
-        // TODO
+        // This application prioritizes retrieving/updating data and does not add/remove elements from the middle.
+        this.movies = new LinkedList<Movie>();
+
     }
 
     public Movie getMovie(int index) {
-        // TODO
-        return null;
+        return this.movies.get(index);
     }
 
     public void setMovie(int index, Movie movie) {
-        // TODO
+       this.movies.set(index, movie);
     }
 
     public void addMovie(Movie movie) {
-        // TODO
+        this.movies.add(movie);
     }
 
     public String toString() {
