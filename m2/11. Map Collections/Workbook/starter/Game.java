83289bb814e6bf971cf7da457dddaaf0b7050999
Jake Lim <jake.lim@ams-osram.com>
2023-02-07 14:41:46 +0800

    12

--- m2/11. Map Collections/Workbook/starter/Game.java
+++ m2/11. Map Collections/Workbook/starter/Game.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Game {
 
@@ -6,25 +7,28 @@ public class Game {
     private LocalDate date;
 
     public Game(String arena) {
-        // TODO
+        // set the arena
+        // set the date equal to the current date
+        this.arena = arena;
+        this.date = LocalDate.now();
     }
 
     public void begin(Team home, Team away) {
-        // TODO
-        System.out.println
-        (
-        "\n - This matchup takes place at the " + this.arena + " arena on " + "<day/month/year>" + "." + 
-        "\n - Tonight's game is between the " + home.getName() + " and the " + away.getName() + ".\n" +
-        "\n - The starting lineup for the home team is: at point guard, " + "<HOME_POINT_GUARD>" + "; at shooting guard, " + "<HOME_SHOOTING_GUARD>" + "; at small forward, " + "<HOME_SMALL_FORWARD>" + "; at power forward, " + "<HOME_POWER_FORWARD>" + "; and at center, " + "<HOME_CENTER>" + ".\n" +
-        "\n - The starting lineup for the visiting team is: at point guard, " + "<AWAY_POINT_GUARD>" + "; at shooting guard, " + "<AWAY_SHOOTING_GUARD>" + "; at small forward, " + "<AWAY_SMALL_FORWARD>" + "; at power forward, " + "<AWAY_POWER_FORWARD>" + "; and at center, " + "<AWAY_CENTER>" +".\n" +
-        "\n - Let's give a warm round of applause for both teams as they take the court!"
-        );
+
+        String formattedDate = this.date.format(DateTimeFormatter.ofPattern("yyy-MM-dd"));
+
+        System.out.println(
+                "\n - This matchup takes place at the " + this.arena + " arena on " + "<" + formattedDate + ">" + "." +
+                        "\n - Tonight's game is between the " + home.getName() + " and the " + away.getName() + ".\n" +
+                        "\n - The starting lineup for the home team is: at point guard, " + home.getPlayer("POINT_GUARD")
+                        + "; at shooting guard, " + home.getPlayer("SHOOTING_GUARD") + "; at small forward, "
+                        + home.getPlayer("SMALL_FORWARD") + "; at power forward, " + home.getPlayer("POWER_FORWARD") + "; and at center, "
+                        + home.getPlayer("CENTER") + ".\n" +
+                        "\n - The starting lineup for the visiting team is: at point guard, " + "<AWAY_POINT_GUARD>"
+                        + "; at shooting guard, " + "<AWAY_SHOOTING_GUARD>" + "; at small forward, "
+                        + "<AWAY_SMALL_FORWARD>" + "; at power forward, " + "<AWAY_POWER_FORWARD>" + "; and at center, "
+                        + "<AWAY_CENTER>" + ".\n" +
+                        "\n - Let's give a warm round of applause for both teams as they take the court!");
     }
 
 }
-
-    
-
-
-
-
