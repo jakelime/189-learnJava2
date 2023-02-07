dde2636bf151c1902c2e70c4c9429f4792f4fd61
Jake Lim <jake.lim@ams-osram.com>
2023-02-07 22:52:47 +0800

    inheritance

--- m2/15. Inheritance/Lesson/starter/Main.java
+++ m2/15. Inheritance/Lesson/starter/Main.java
@@ -1,8 +1,15 @@
+import product.Shirt;
+
 public class Main {
 
     public static void main(String[] args) {
+        Shirt shirt = new Shirt();
+        shirt.setSize(Shirt.Size.SMALL);
+        shirt.setBrand("Nike");
+        shirt.setPrice(49.99);
+        shirt.setColor("BLUE");
 
-
+        shirt.fold();
 
     }
 
