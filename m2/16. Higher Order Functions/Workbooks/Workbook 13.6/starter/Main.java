a1eb9c8c859e8e20a681958fcdcccb2a8899bc4b
Jake Lim <jake.lim@ams-osram.com>
2023-02-10 15:18:02 +0800

    lessons completed

--- m2/16. Higher Order Functions/Workbooks/Workbook 13.6/starter/Main.java
+++ m2/16. Higher Order Functions/Workbooks/Workbook 13.6/starter/Main.java
@@ -1,3 +1,4 @@
+import java.security.cert.X509CRL;
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,26 +7,33 @@ public class Main {
         List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "Dave");
 
         System.out.println("\n**************************************");
-        // names.stream()
-            
-   
+        names.stream()
+            .map(x -> "Hello! " + x)
+            .forEach(x -> System.out.println(x));
+
+
         System.out.println("\n**************************************");
-        
+
         List<String> usernames = Arrays.asList("sparklingunicorn", "galactic_goddess", "neon_ninja", "purplepixiedust");
-     
-        // String username =  usernames.stream();
-     
-        // System.out.println("Found you: " + username);
+
+        String targetUsername = "neon_ninja";
+        String username =  usernames.stream()
+            .filter(x -> x.equals(targetUsername))
+            .findFirst()
+            .orElse("usernameNotFound");
+        System.out.println("Found you: " + username);
 
         System.out.println("\n**************************************");
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-        // long numEven = numbers.stream()
-        
-        // System.out.println("There are " + numEven + " even numbers in this list");
-        
-    }   
-    
+        long numEven = numbers.stream()
+            .filter(x -> x % 2 == 0)
+            .count();
+
+        System.out.println("There are " + numEven + " even numbers in this list");
+
+    }
+
 }
 
 
--- m2/16. Higher Order Functions/Workbooks/Workbook 13.7/starter/Main.java
+++ m2/16. Higher Order Functions/Workbooks/Workbook 13.7/starter/Main.java
@@ -1,10 +1,16 @@
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("emails.txt");
-        
+
+        Files.lines(path)
+                .filter(x -> !x.startsWith("Spam"))
+                .forEach(x -> System.out.println(x));
+
     }
 }
